fc4deee3418e630fcbf0278459b1726b9c812458
myclamm <mikelam231@gmail.com>
2014-07-31 13:39:34 -0700

    first attempt at rooksolution

--- src/solvers.js
+++ src/solvers.js
@@ -13,37 +13,89 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-window.findNRooksSolution = function(n) {
+window.findNRooksSolution = function(n,array) {
+  // console.log(array);
+  if(!array){
+    var emptyBoard =  new Board({'n':n});
+    var Matrix = emptyBoard.rows();
+  } else{
+    var Matrix = array;
+    var emptyBoard = new Board(Matrix);
+  }
+  //iterate over rows
+  for(var row = 0;row<emptyBoard.rows().length;row++){
+    var thisRow = emptyBoard.get(row);
+    //iterate over cells in row
+    for(var cell = 0;cell<thisRow.length;cell++){
 
-
-//HOW TO OUTPUT A WORKING ROOK SOLUTION
- //the function takes a Matrix as input
-//start with inputting a Matrix of one row with length n, with index 0 = 1
-//if the Matrix hasNoConflicts && Matrix.length = n, return the Matrix;
-//otherwise insert a new row with 1 at index 0.
-  //for every cell in this new row,
-    //change the cell to 1, check if the Matrix has conflicts.
-    //if it doesn't have conflicts, return the function again passing in the new board
-
-//call the function once for ever position of 1...or something
-
-[[1,0,0,0],
-[0,1,0,0],
-[]]
-
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+      if(!Matrix[row][cell]){
+        Matrix[row][cell] = 1;
+        var emptyBoard = new Board(Matrix);
+        if(emptyBoard.hasAnyRowConflicts() || emptyBoard.hasAnyColConflicts()){
+          Matrix[row][cell] = 0;
+          var emptyBoard = new Board(Matrix);
+        }
+      }
     }
+  }
+  var solution = emptyBoard.rows();
+  if(!array){
+    console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+    return solution;
+  } else {
+    return solution;
+  }
+
 };
 
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var storage = [];
+  var emptyMatrix = new Board({'n':n});
+  var emptyMatrix = emptyMatrix.rows();
+  //iterate over rows of emptyMAtrix
+  for(var rows=0;rows<emptyMatrix.length;rows++){
+    var thisRow = emptyMatrix[rows];
+    //now we are ready to iterate over every cell of the matrix
+    for(var cell = 0;cell<thisRow.length;cell++){
+      //create a Matrix instance for unique cell starting point
+      var newBoard = new Board({'n':n});
+      var newMatrix = newBoard.rows();
+      //console.log("before"+" "+newMatrix)
+      newMatrix[rows][cell] = 1;
+      // console.log(rows+" "+cell);
+      //store a solution for the unique Matrix
+      //debugger;
+      //console.log("after"+" "+newMatrix);
+      var answer = this.findNRooksSolution(n,newMatrix);
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+      storage.push(JSON.stringify(answer));
+      var inverted = [];
+      for(var g=0;g<answer.length;g++){
+        inverted.push(answer[g].slice().reverse());
+      }
+      storage.push(JSON.stringify(inverted));
+      var superInverted = inverted.slice().reverse();
+      storage.push(JSON.stringify(superInverted))
+
+      var verticalInvert = answer.slice().reverse();
+      storage.push(JSON.stringify(verticalInvert));
+
+    }
+  }
+  //check storage for duplicates
+
+  var apples =  _.uniq(storage);
+  console.log(storage)
+
+
+  var solutionCount = apples.length; //fixme
+
+  for(var i=0;i<apples.length;i++){
+    console.log(apples[i]+"")
+  }
   return solutionCount;
 };
 
@@ -57,7 +109,7 @@ window.findNQueensSolution = function(n) {
   return solution;
 };
 
-
+//****consider flattenign the array for optimization
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
   var solutionCount = undefined; //fixme
